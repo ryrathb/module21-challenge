7ca70c721da211f9f6c66df2cde748115c28ffdc
Ryan Rathbun <ryrathb@arizona.edu>
2022-08-30 18:59:04 -0700

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
 
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
 
 const userClient = new ApolloClient({
-  request: (action) => {
-    const token = localStorage.getItem("id_token");
-
-    action.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
-  uri: "/graphql",
+  link: authLink.concat(httpLink)
 });
 
 function App() {
@@ -28,20 +31,12 @@ function App() {
     <Router>
       <>
         <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
+        <Switch>
+          <Route path="/" component={SearchBooks} />
+          <Route path="/saved" element={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1> }
           />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
+        </Switch>
       </>
     </Router>
     </ApolloProvider>
